2f9a7e180aa9eed49bb9195ed3db937fdb991680
mokurin000 <1348292515a@gmail.com>
2025-03-05 05:06:05 +0800

    fix: should make piped stdout to capture

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error, net::Ipv6Addr, str::FromStr};
+use std::{env, error::Error, net::Ipv6Addr, process::Stdio, str::FromStr};
 
 const MAGIC_COMMAND: &str =
     r#"ubus call network.interface.wan_6 status | jq -r '.["ipv6-prefix"][0].address'"#;
@@ -7,12 +7,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let output = std::process::Command::new("/bin/sh")
         .arg("-c")
         .arg(MAGIC_COMMAND)
+        .stdout(Stdio::piped())
         .spawn()?
         .wait_with_output()?
         .stdout;
     let pd_prefix = String::from_utf8_lossy(&output).to_string();
 
-    let pd_ip = Ipv6Addr::from_str(pd_prefix.trim())?;
+    let pd_ip = Ipv6Addr::from_str(&pd_prefix.trim())?;
     let suffix_ip = Ipv6Addr::from_str(&env::var("IPV6_SUFFIX")?)?;
 
     let result = pd_ip.to_bits() | suffix_ip.to_bits();
