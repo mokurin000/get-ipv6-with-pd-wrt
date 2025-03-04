9d2f161a935db0a93bbc300cce59eb34b69dc87c
mokurin000 <1348292515a@gmail.com>
2025-03-05 05:26:51 +0800

    enhance: drop dependency `jq`

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use std::{env, error::Error, net::Ipv6Addr, process::Stdio, str::FromStr};
 
-const MAGIC_COMMAND: &str =
-    r#"ubus call network.interface.wan_6 status | jq -r '.["ipv6-prefix"][0].address'"#;
+use serde_json::Value;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let output = std::process::Command::new("/bin/sh")
-        .arg("-c")
-        .arg(MAGIC_COMMAND)
+    let output = std::process::Command::new("/bin/ubus")
+        .arg("call")
+        .arg("network.interface.wan_6")
+        .arg("status")
         .stdout(Stdio::piped())
         .spawn()?
         .wait_with_output()?
         .stdout;
-    let pd_prefix = String::from_utf8_lossy(&output).to_string();
 
-    let pd_ip = Ipv6Addr::from_str(&pd_prefix.trim())?;
+    let result_json: Value = serde_json::from_slice(&output)?;
+    let prefix = result_json
+        .pointer("/ipv6-prefix/0/address")
+        .ok_or("failed to parse delegated prefix")?
+        .as_str()
+        .ok_or("delegated prefix must be string")?;
+
+    let pd_ip = Ipv6Addr::from_str(&prefix)?;
     let suffix_ip = Ipv6Addr::from_str(&env::var("IPV6_SUFFIX")?)?;
 
     let result = pd_ip.to_bits() | suffix_ip.to_bits();
