87b857a184f2fb5093e670b60d8caaf5c68a8327
mokurin000 <1348292515a@gmail.com>
2025-03-05 02:37:15 +0800

    feat: implement full logic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use std::{error::Error, net::Ipv6Addr, process::Command, str::FromStr};
-
-const MAGIC_COMMAND: &str =
-    r#"ubus call network.interface.wan_6 status | jq -r '.["ipv6-prefix"][0].address'"#;
+use std::{env::args, error::Error, net::Ipv6Addr, str::FromStr};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let pd_prefix = Command::new("/bin/sh")
-        .arg("-c")
-        .arg(MAGIC_COMMAND)
-        .spawn()?
-        .wait_with_output()?
-        .stdout;
-    let pd_prefix = String::from_utf8_lossy(&pd_prefix);
+    let pd_prefix;
+
+    #[cfg(not(target_os = "linux"))]
+    {
+        pd_prefix = "ffff:ffff:ffff:ffff::";
+    }
+    #[cfg(target_os = "linux")]
+    {
+        const MAGIC_COMMAND: &str =
+            r#"ubus call network.interface.wan_6 status | jq -r '.["ipv6-prefix"][0].address'"#;
+        let output = std::process::Command::new("/bin/sh")
+            .arg("-c")
+            .arg(MAGIC_COMMAND)
+            .spawn()?
+            .wait_with_output()?
+            .stdout;
+        pd_prefix = String::from_utf8_lossy(&output);
+    }
+
     let pd_ip = Ipv6Addr::from_str(pd_prefix.trim())?;
-    println!("{pd_ip}");
+
+    let ipv6_suffix = args()
+        .nth(1)
+        .expect(&format!("usage: {} <ipv6_suffix>", args().nth(0).unwrap()));
+    let suffix_ip = Ipv6Addr::from_str(&ipv6_suffix)?;
+
+    let result = pd_ip.to_bits() | suffix_ip.to_bits();
+    println!("{}", Ipv6Addr::from_bits(result));
 
     Ok(())
 }
